517760157a01c0503aba8a287107b303c5a6434a
Sbarrasa <zaiper2000@gmail.com>
2025-03-18 04:18:00 -0300

    Simplificación de ObjectMatcher (se quitó filtro de colecciones. Se cambió por uso de streams)

--- src/main/java/com/sbarrasa/bank/matcher/ObjectMatcher.java
+++ src/main/java/com/sbarrasa/bank/matcher/ObjectMatcher.java
@@ -1,7 +1,6 @@
 package com.sbarrasa.bank.matcher;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class ObjectMatcher<T> {
 
@@ -12,11 +11,6 @@ public class ObjectMatcher<T> {
     this.getters = new HashSet<>(Arrays.asList(getters));
   }
 
-  public List<T> filter(Collection<T> objects, T sample, Match matchType){
-    return objects.stream()
-      .filter(object -> match(object, sample, matchType))
-      .collect(Collectors.toList());
-  }
 
   public boolean match(T object, T sampleObject, Match matchType) {
     for (Getter<T> getter : getters) {
--- src/test/java/com/sbarrasa/bank/product/ProductTest.java
+++ src/test/java/com/sbarrasa/bank/product/ProductTest.java
@@ -5,6 +5,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
+  Product productTC_VISA = new Product()
+    .setProductType(ProductType.TC)
+    .setBranch(Branch.VISA)
+    .setTier("GOLD")
+    .setCreditLimit(6000000.00);
+
+  Product productTC_AMEX = new Product()
+    .setProductType(ProductType.TC)
+    .setBranch(Branch.AMEX)
+    .setCreditLimit(5000000.00);
+
+  Product productTD = new Product()
+    .setProductType(ProductType.TD)
+    .setBranch(Branch.VISA);
+
+  Product productCC = new Product()
+    .setProductType(ProductType.CC)
+    .setCurrency(Currency.ARS)
+    .setCreditLimit(1000000.00);
+
+  Product productCA_USD = new Product()
+    .setProductType(ProductType.CA)
+    .setCurrency(Currency.USD);
 
   @Test
   void anyMatch() {
@@ -12,10 +35,10 @@ class ProductTest {
       .setBranch(Branch.VISA)
       .setProductType(ProductType.TC);
 
-    assertTrue(SampleProducts.productTC_VISA.match(sample, Match.ANY));
-    assertTrue(SampleProducts.productTD.match(sample, Match.ANY));
-    assertTrue(SampleProducts.productTC_AMEX.match(sample, Match.ANY));
-    assertFalse(SampleProducts.productCC.match(sample, Match.ANY));
+    assertTrue(productTC_VISA.match(sample, Match.ANY));
+    assertTrue(productTD.match(sample, Match.ANY));
+    assertTrue(productTC_AMEX.match(sample, Match.ANY));
+    assertFalse(productCC.match(sample, Match.ANY));
 
   }
 
@@ -24,9 +47,9 @@ class ProductTest {
     var sample = new Product()
       .setBranch(Branch.VISA);
 
-    assertTrue(SampleProducts.productTC_VISA.match(sample, Match.ALL));
-    assertTrue(SampleProducts.productTD.match(sample, Match.ALL));
-    assertFalse(SampleProducts.productTC_AMEX.match(sample, Match.ALL));
+    assertTrue(productTC_VISA.match(sample, Match.ALL));
+    assertTrue(productTD.match(sample, Match.ALL));
+    assertFalse(productTC_AMEX.match(sample, Match.ALL));
 
   }
 
@@ -35,17 +58,17 @@ class ProductTest {
     var sample = new Product()
       .setIsCredit(true);
 
-    assertTrue(SampleProducts.productCC.match(sample, Match.ALL));
-    assertTrue(SampleProducts.productTC_VISA.match(sample, Match.ALL));
-    assertFalse(SampleProducts.productCA_USD.match(sample, Match.ALL));
+    assertTrue(productCC.match(sample, Match.ALL));
+    assertTrue(productTC_VISA.match(sample, Match.ALL));
+    assertFalse(productCA_USD.match(sample, Match.ALL));
 
   }
 
 
   @Test
   void getName(){
-    assertEquals(ProductType.TD.getDescription(), SampleProducts.productTD.getName());
-    assertEquals(ProductType.TC.getDescription(), SampleProducts.productTC_VISA.getName());
+    assertEquals(ProductType.TD.getDescription(), productTD.getName());
+    assertEquals(ProductType.TC.getDescription(), productTC_VISA.getName());
 
   }
 }
